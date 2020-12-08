77266a64b7370799fcb8772475a9f58c854f783a
Stefano Rovida <stefanorovida@Stefanos-MBP.fritz.box>
2020-12-08 18:18:06 +0100

    maybe better

--- mainServer.js
+++ mainServer.js
@@ -8,25 +8,30 @@ import {CmdHandler} from './app/handlers/CmdHandler';
 
 client.once('ready', () => {
   console.log('ready');
+
   client.on('message', message => {
-    if (message.author.bot) {
-      console.log('bot, no answer lel');
-      return;
-    }
-    console.log('message received');
+    try {
+      if (message.author.bot) {
+        console.log('bot, no answer lel');
+        return;
+      }
+      console.log('message received');
 
-    // Voice only works in guilds, if the message does not come from a guild,
-    // we ignore it
-    if (!message.guild) {
-      message.reply("I Can't join the channel because this is not a guild, sorry :/");
-      return;
-    }
+      // Voice only works in guilds, if the message does not come from a guild,
+      // we ignore it
+      if (!message.guild) {
+        message.reply("I Can't join the channel because this is not a guild, sorry :/");
+        return;
+      }
 
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      CmdHandler.CheckPrefix(message, client);
-    } else {
-      message.reply('You need to join a voice channel first!');
+      // Only try to join the sender's voice channel if they are in one themselves
+      if (message.member.voiceChannel) {
+        CmdHandler.CheckPrefix(message, client);
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+    } catch (e) {
+      console.error(e);
     }
 
   });
